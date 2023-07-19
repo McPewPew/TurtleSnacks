426b2f1631457da5e0479963484e41d9ac967ab6
Lexie <6937574+Lexiebean@users.noreply.github.com>
2023-07-19 12:33:03 +1000

    Added support for the upcoming radio button

--- TurtleSnacks.lua
+++ TurtleSnacks.lua
@@ -3,6 +3,7 @@ frame:RegisterEvent("VARIABLES_LOADED");
 frame:SetScript("OnEvent",function(self,event,...)
 	TWMinimapShopFrame:ClearAllPoints() TWMinimapShopFrame:SetParent(Minimap) TWMinimapShopFrame:SetPoint("LEFT",Minimap,"RIGHT",-1, -1)
 	TWMiniMapBattlefieldFrame:ClearAllPoints() TWMiniMapBattlefieldFrame:SetParent(Minimap) TWMiniMapBattlefieldFrame:SetPoint("LEFT",Minimap,"RIGHT",-1, -1)
+	EBC_Minimap:ClearAllPoints() EBC_Minimap:SetParent(Minimap) EBC_Minimap:SetPoint("LEFT",Minimap,"RIGHT",-1, -1)
 	if (IsAddOnLoaded("LFT")) then
 		LFT_Minimap:ClearAllPoints() LFT_Minimap:SetParent(Minimap) LFT_Minimap:SetPoint("LEFT",Minimap,"RIGHT",-1, -1)
 	end
