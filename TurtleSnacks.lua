0afe446c281427658de7feb125a6e0269cf9b87d
McPewPew <98543024+McPewPew@users.noreply.github.com>
2024-11-17 16:33:17 +0000

    Add files via upload

--- TurtleSnacks.lua
+++ TurtleSnacks.lua
@@ -1,6 +1,6 @@
 	DEFAULT_CHAT_FRAME:AddMessage("Don't worry TurtleSnacks has fed your turtles", 1, 0, 1);
-if (TWMinimapShopFrame~=nil) then
-	TWMinimapShopFrame:ClearAllPoints() MinimapShopFrame:SetParent(Minimap) TWMinimapShopFrame:SetPoint("LEFT",Minimap,"RIGHT",-1, -1)
+if (MinimapShopFrame~=nil) then
+	MinimapShopFrame:ClearAllPoints() MinimapShopFrame:SetParent(Minimap) MinimapShopFrame:SetPoint("LEFT",Minimap,"RIGHT",-1, -1)
 end
 if (TWMiniMapBattlefieldFrame~=nil) then
 	TWMiniMapBattlefieldFrame:ClearAllPoints() TWMiniMapBattlefieldFrame:SetParent(Minimap) TWMiniMapBattlefieldFrame:SetPoint("LEFT",Minimap,"RIGHT",-1, -1)
